7339abb6b100a1c6ab7948d608c6b5dce5a2c6fd
ashishkumarmeena179-cyber <ashishkumarmeena179@gmail.com>
2026-02-24 21:22:44 +0530

    Create index.js

---
+++ src/i18n/index.js
@@ -0,0 +1,15 @@
+import en from './translations/en';
+import hi from './translations/hi';
+import es from './translations/es';
+import fr from './translations/fr';
+import it from './translations/it';
+import ko from './translations/ko';
+
+export const translations = {
+  en,
+  hi,
+  es,
+  fr,
+  it,
+  ko,
+};
